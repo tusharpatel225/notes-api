d94a8b5862b235ef529120615c545e47efe5bef9
tusharpatel225 <tusharkumarpatel.dcs17@vnsgu.ac.in>
2019-01-08 16:24:53 +0530

    after cors

--- server/server.js
+++ server/server.js
@@ -10,6 +10,14 @@ const bodyParser  = require('body-parser');
 const port = process.env.PORT;
 const app = express();
 
+
+app.all('*', function(req, res, next) {
+     var origin = req.get('origin');
+     res.header('Access-Control-Allow-Origin', origin);
+     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+     res.header('Access-Control-Allow-Headers', 'Content-Type');
+     next();
+});
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
